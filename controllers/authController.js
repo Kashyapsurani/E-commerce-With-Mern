bd27b48164b96a61eed79edd06322c21fa5bf306
Kashyap surani <152683782+Kashyapsurani@users.noreply.github.com>
2025-04-10 16:44:04 +0530

    Exam

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,9 +3,28 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 
 exports.register = async (req, res) => {
-  await User.create(req.body);
-  res.redirect("/login");
-}; 
+  try {
+    // Check if the username already exists
+    const existingUser = await User.findOne({ username: req.body.username });
+
+    if (existingUser) {
+      // If username exists, render the registration page again with an error message
+      return res.render("register", {
+        errorMessage: "Username already exists. Please choose a different one.",
+      });
+    }
+
+    // If username doesn't exist, create the new user
+    await User.create(req.body);
+    res.redirect("/login");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error. Please try again later.");
+  }
+};
+
+
+
 
 exports.login = async (req, res) => {
   const user = await User.findOne({ username: req.body.username });
